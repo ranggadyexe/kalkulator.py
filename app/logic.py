8e5e1c65d3680fb70261d7f7255c8192eef8f159
Rangga Dewa <ranggadewa853@gmail.com>
2025-04-16 16:04:36 +0700

    pusing

--- app/logic.py
+++
@@ -1,8 +0,0 @@
-# app/logic.py
-
-class CalculatorLogic:
-    def safe_eval(self, expression):
-        try:
-            return eval(expression, {"__builtins__": None}, {})
-        except Exception:
-            return "Error"
